94a4d7cd092f4fb83f47386718273c6d72c01cd8
EhsanTgv <EhsanTgv@gmail.com>
2022-03-03 16:34:23 +0300

    Initial Commit

---
+++ SwiftUICalculatorPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICalculatorPractice
+//
+//  Created by İbrahim on 3.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICalculatorPractice/SwiftUICalculatorPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorPracticeApp.swift
+//  SwiftUICalculatorPractice
+//
+//  Created by İbrahim on 3.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
