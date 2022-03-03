6b1422e8e98dc03571e13bb4aac6ded589fdf7c2
EhsanTgv <EhsanTgv@gmail.com>
2022-03-03 17:20:00 +0300

    Implement Design

--- SwiftUICalculatorPractice/ContentView.swift
+++ SwiftUICalculatorPractice/ContentView.swift
@@ -8,10 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    let grid = [
+        ["AC","Del","%","/"],
+        ["7","8","9","X"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        [".","0","","="],
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Spacer()
+                Text("Hello, world!")
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 30, weight: .heavy))
+            }
+            HStack{
+                Spacer()
+                Text("Hello, world!")
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 50, weight: .heavy))
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        
+        ForEach(grid,id:\.self) {
+            row in
+            HStack{
+                ForEach(row, id: \.self) {
+                    cell in
+                    Button(action: {}, label: {
+                        Text(cell)
+                            .foregroundColor(.white)
+                            .font(.system(size: 40,weight:.heavy))
+                            .frame(maxWidth:.infinity,maxHeight:.infinity)
+                    })
+                }
+            }
+        }
+        
+        .background(Color.black.ignoresSafeArea())
     }
+    
+//    func buttonPressed(cell:String) {
+//
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
