04c9269e94200a1e5146c3d4d027cdfae75dcd79
EhsanTgv <EhsanTgv@gmail.com>
2022-03-06 23:52:04 +0300

    Calculate result

--- SwiftUICalculatorPractice/ContentView.swift
+++ SwiftUICalculatorPractice/ContentView.swift
@@ -15,45 +15,132 @@ struct ContentView: View {
         ["1","2","3","+"],
         [".","0","","="],
     ]
+    
+    let operators = ["/","+","X","%"]
+    
+    @State var visibleWorkings = ""
+    @State var visibleResults = ""
+    @State var showAlert = false
+    
     var body: some View {
         VStack{
             HStack{
                 Spacer()
-                Text("Hello, world!")
+                Text(visibleWorkings)
                     .padding()
                     .foregroundColor(Color.white)
                     .font(.system(size: 30, weight: .heavy))
             }
             HStack{
                 Spacer()
-                Text("Hello, world!")
+                Text(visibleResults)
                     .padding()
                     .foregroundColor(Color.white)
                     .font(.system(size: 50, weight: .heavy))
             }.frame(maxWidth: .infinity, maxHeight: .infinity)
-        }
+        }.background(Color.black.ignoresSafeArea())
         
-        ForEach(grid,id:\.self) {
+        ForEach(grid, id: \.self) {
             row in
             HStack{
                 ForEach(row, id: \.self) {
                     cell in
                     Button(action: {}, label: {
                         Text(cell)
-                            .foregroundColor(.white)
+                            .foregroundColor(buttonColor(cell))
                             .font(.system(size: 40,weight:.heavy))
                             .frame(maxWidth:.infinity,maxHeight:.infinity)
                     })
                 }
             }
         }
-        
         .background(Color.black.ignoresSafeArea())
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Invalid Input"), message: Text(visibleWorkings), dismissButton: .default(Text("Okay"))
+            )
+        }
     }
     
-//    func buttonPressed(cell:String) {
-//
-//    }
+    func buttonColor(_ cell: String) -> Color {
+        if cell == "AC" || cell == "Del" {
+            return .red
+        }
+        
+        if cell == "-" || cell == "=" || operators.contains(cell) {
+            return .orange
+        }
+        
+        return .white
+    }
+    
+    func buttonPressed(cell:String) {
+        switch cell {
+        case "AC":
+            visibleWorkings = ""
+            visibleResults = ""
+        case "Del":
+            visibleWorkings = String(visibleWorkings.dropLast())
+        case "=":
+            visibleResults = calculateResults()
+        case "-":
+            addMinus()
+        case "X", "/", "%", "+":
+            addOperator(cell)
+        default:
+            visibleWorkings += cell
+        }
+    }
+    
+    func addOperator(_ cell: String) {
+        if !visibleWorkings.isEmpty {
+            let last = String(visibleWorkings.last!)
+            if operators.contains(last) || last == "-" {
+                visibleWorkings.removeLast()
+            }
+            visibleWorkings += cell
+        }
+    }
+    
+    func addMinus() {
+        if visibleWorkings.isEmpty || visibleWorkings.last! != "-" {
+            visibleWorkings += "-"
+        }
+    }
+    
+    func calculateResults() -> String {
+        if validInput() {
+            var workings = visibleWorkings.replacingOccurrences(of: "%", with: "*0.01")
+            workings = visibleWorkings.replacingOccurrences(of: "X", with: "*")
+            let expression = NSExpression(format: workings)
+            let result = expression.expressionValue(with: nil, context: nil) as! Double
+            return formatResult(val: result)
+        }
+        showAlert = true
+        return ""
+    }
+    
+    func validInput() -> Bool {
+        if visibleResults.isEmpty {
+            return false
+        }
+        let last = String(visibleWorkings.last!)
+        
+        if operators.contains(last) || last == "-" {
+            if last != "%" || visibleWorkings.count == 1 {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func formatResult(val: Double) -> String {
+        if val.truncatingRemainder(dividingBy: 1) == 0 {
+            return String(format: "%.0f", val)
+        }
+        return String(format: "%.2f", val)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
